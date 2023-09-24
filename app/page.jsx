7ffb91d917a4d94f0596aa0988cf0d4e5f9026b6
heyLowk <heylowk@gmail.com>
2023-09-24 17:09:21 +0200

    app

---
+++ app/page.jsx
@@ -0,0 +1,27 @@
+import Movie from "./movie"
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+  console.log(res)
+
+  return (
+    <main>
+      <h1 className="text-8xl">Want to watch a Movie?</h1>
+      <h1 className="text-lg py-10">Here you have the most popular ones</h1>
+      <div className="grid gap-16 grid-cols-fluid">
+        {res.results.map((movie) => (
+          <Movie 
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+        ))}
+      </div>
+    </main>
+  )
+}
